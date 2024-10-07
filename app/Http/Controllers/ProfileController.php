de2719ce2f6e6ad547bda2eddd376ba532b7e12d
Saif Kamal <kamal.saifkamal534@gmail.com>
2024-10-06 18:19:20 -0700

    Added Post Image, Profile Image & Search Functionality(2)

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\{DB,Hash};
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController
 {
